a25b5c507232f77926894b6e5187d0b3a7d80ee1
abhi-jithb <babhijith58@gmail.com>
2025-08-16 22:31:17 +0530

    Added Temperture converter and added gitignore file

--- main.py
+++ main.py
@@ -15,12 +15,29 @@ def root():
 def home_weather():
     return {"cities": SUPPORTED_CITIES}
 
+
 @app.get("/weather/{city}")
-def get_weather(city: str):
+def get_weather(city: str, unit: str = "C"):
     city = city.lower()
     if city not in SUPPORTED_CITIES:
-       return {"error": "City not supported"}
-    return WEATHER_DATA[city]
+        return {"error": "City not supported"}
+    
+    weather = WEATHER_DATA[city]
+    temp_c = weather["temp"]
+
+    if unit.upper() == "F":
+        temp = temp_c * 9 / 5 + 32
+    else:
+        temp = temp_c
+
+    return {
+        "city": city,
+        "temp": round(temp, 2),
+        "unit": unit.upper(),
+        "humidity": weather["humidity"],
+        "condition": weather["condition"]
+    }
+
 
 @app.get("/weather/{city}/details")
 def get_weather_details(city: str):
